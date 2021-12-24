14a309d8169c435f5780ca0f9a958e6fc61a3187
Nandar Sukmana <nsukmana.dev@gmail.com>
2021-12-24 09:13:28 +0700

    Library testify with assert & require

--- helper/hello_world.go
+++ helper/hello_world.go
@@ -1,5 +1,5 @@
 package helper
 
 func HelloWorld(name string) string {
-	return "Hello hai " + name
+	return "Hai " + name
 }
--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -3,6 +3,9 @@ package helper
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestHelloWorld(t *testing.T) {
@@ -23,4 +26,18 @@ func TestHelloWorldSukmana(t *testing.T) {
 	}
 
 	fmt.Println("Unit test TestHelloWorldSukmana done")
+}
+
+func TestHelloWorldRequire(t *testing.T) {
+	result := HelloWorld("Nandar Sukmana Require")
+	require.Equal(t, "Hello Nandar Sukmana Require", result, "Result must be 'Hello Nandar Sukmana Require'")
+
+	fmt.Println("Unit test TestHelloWorldRequire done")
+}
+
+func TestHelloWorldAssertion(t *testing.T) {
+	result := HelloWorld("Nandar Sukmana")
+	assert.Equal(t, "Hello Nandar Sukmana", result, "Result must be 'Hello Nandar Sukmana'")
+
+	fmt.Println("Unit test TestHelloWorldAssertion done")
 }
